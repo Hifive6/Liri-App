bd696c3127889ce3331abfb21b0e68c8310bda09
Hifive6 <39879994+Hifive6@users.noreply.github.com>
2018-09-03 20:59:22 -0400

    got the bands url to work

--- liri.js
+++ liri.js
@@ -1,59 +1,86 @@
 require("dotenv").config();
-var request = require("request");
+
 // var spotify = require("./keys.js");
 
+var request = require("request");
 
-var nodeArgs = process.argv
-var movieName = "";
-for(var i=2; i< nodeArgs.length; i++){
-    if (i > 2 && i < nodeArgs.length){
-        movieName = movieName + "+" + nodeArgs[i];
+// var nodeArgs = process.argv
+// var movieName = "";
+// for(var i=2; i< nodeArgs.length; i++){
+//     if (i > 2 && i < nodeArgs.length){
+//         movieName = movieName + "+" + nodeArgs[i];
+//     }else{
+//         movieName += nodeArgs[i];
+//     }
+// }
+
+// var movieUrl = "http://www.omdbapi.com/?t="+ movieName + "&apikey=trilogy"
+
+// request(movieUrl, function(error, response, body){
+    
+//     if(!error && response.statusCode === 200){
+//         var movieInfo = JSON.parse(body);
+//         if (!movieName){
+//             nobodyUrl = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy"
+//             request(nobodyUrl, function(error, response, body){
+//                 if(!error && response.statusCode === 200){
+//                     var nobodyMovie = JSON.parse(body);
+//                     console.log(`
+//         Title:                    ${nobodyMovie['Title']} (${nobodyMovie['Year']})
+//         IMDB Rating:              ${nobodyMovie['imdbRating']}
+//         Rotten Tomatoes Rating:   ${nobodyMovie['Ratings'][1]['Value']}
+//         Country Produced in:      ${nobodyMovie['Country']}
+//         Language:                 ${nobodyMovie['Language']}
+//         Actors:                   ${nobodyMovie['Actors']}
+//         Plot:                     ${nobodyMovie['Plot']}
+          
+//                     `);
+//                 }
+//             })
+            
+//             console.log("If you haven't Watch Mr.Nobody you should, it's on Netflix!")
+//         }else{
+       
+//        console.log(`
+//        Title:                    ${movieInfo['Title']} (${movieInfo['Year']})
+//        IMDB Rating:              ${movieInfo['imdbRating']}
+//        Rotten Tomatoes Rating:   ${movieInfo['Ratings'][1]['Value']}
+//        Country Produced in:      ${movieInfo['Country']}
+//        Language:                 ${movieInfo['Language']}
+//        Actors:                   ${movieInfo['Actors']}
+//        Plot:                     \n${movieInfo['Plot']}
+            
+//        `)
+//     }
+    
+//     }
+// })
+
+var bandArgs = process.argv;
+var artist = "";
+
+for(var i=2; i< bandArgs.length; i++){
+    if (i > 2 && i < bandArgs.length){
+        artist = artist + "+" + bandArgs[i];
     }else{
-        movieName += nodeArgs[i];
+        artist += bandArgs[i];
     }
 }
 
-var queryUrl = "http://www.omdbapi.com/?t="+ movieName + "&apikey=trilogy"
+var bandUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 
-request(queryUrl, function(error, response, body){
+request(bandUrl, function(error, response, body){
     
     if(!error && response.statusCode === 200){
-        var movieInfo = JSON.parse(body);
-        if (!movieName){
-            nobodyUrl = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy"
-            request(nobodyUrl, function(error, response, body){
-                if(!error && response.statusCode === 200){
-                    var nobodyMovie = JSON.parse(body);
-                    console.log(`
-        Title:                    ${nobodyMovie['Title']} (${nobodyMovie['Year']})
-        IMDB Rating:              ${nobodyMovie['imdbRating']}
-        Rotten Tomatoes Rating:   ${nobodyMovie['Ratings'][1]['Value']}
-        Country Produced in:      ${nobodyMovie['Country']}
-        Language:                 ${nobodyMovie['Language']}
-        Actors:                   ${nobodyMovie['Actors']}
-        Plot:                     ${nobodyMovie['Plot']}
-          
-                    `);
-                }
-            })
-            
-            console.log("If you haven't Watch Mr.Nobody you should, it's on Netflix!")
-        }else{
-       
-       console.log(`
-       Title:                    ${movieInfo['Title']} (${movieInfo['Year']})
-       IMDB Rating:              ${movieInfo['imdbRating']}
-       Rotten Tomatoes Rating:   ${movieInfo['Ratings'][1]['Value']}
-       Country Produced in:      ${movieInfo['Country']}
-       Language:                 ${movieInfo['Language']}
-       Actors:                   ${movieInfo['Actors']}
-       Plot:                     \n${movieInfo['Plot']}
-            
-       `)
-    }
-    
+        var artistInfo = JSON.parse(body);
+        console.log(artistInfo);
+
     }
 })
+
+
+
+
 
 
 
