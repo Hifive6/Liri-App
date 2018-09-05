22241b5e144eec2b445f4e3196bb51af4409463c
Hifive6 <39879994+Hifive6@users.noreply.github.com>
2018-09-05 13:59:45 -0400

    beginning the spotify part

--- keys.js
+++ keys.js
@@ -2,5 +2,7 @@ console.log('this is loaded');
 
 exports.spotify = {
   id: process.env.d3ee4ebef39140049a7f857cc69a59e7,
-  secret: process.env.b2609640848eafb60769679e53
+  secret: process.env.b2609640848eafb60769679e53,
+  
+  // b2609640848eafb60769679e53
 };
--- liri.js
+++ liri.js
@@ -1,101 +1,105 @@
 require("dotenv").config();
 
-// var spotify = require("./keys.js");
+var keyFile = require("./keys.js");
+console.log(keyFile);
+var spotify = new Spotify(keys.spotify)
 
-var request = require("request");
-var inputString = process.argv;
-var operand = inputString[2];
-var userInput = inputString[3];
+console.log(spotify)
+
+// var request = require("request");
+// var inputString = process.argv;
+// var operand = inputString[2];
+// var userInput = inputString[3];
 
 
-var inputName = "";
-for(var i=0; i< userInput.length; i++){
-    if (i > 3 && i < userInput.length){
-        inputName = inputName + "+" + userInput[i];
+// var inputName = "";
+// for(var i=0; i< userInput.length; i++){
+//     if (i > 3 && i < userInput.length){
+//         inputName = inputName + "+" + userInput[i];
         
-    }else{
-        inputName += userInput[i];
-        
-    }
-}
-
-if (operand === "movie-this"){
-    movieIt();
-}else if(operand === "concert-this"){
-    concertIt();
-
-}
-function movieIt(){
-var movieUrl = "http://www.omdbapi.com/?t="+ inputName + "&apikey=trilogy"
-
-request(movieUrl, function(error, response, body){
-    
-    if(!error && response.statusCode === 200){
-        var movieInfo = JSON.parse(body);
-        if (inputName == false){
-            nobodyUrl = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy"
-            request(nobodyUrl, function(error, response, body){
-                if(!error && response.statusCode === 200){
-                    var nobodyMovie = JSON.parse(body);
-                    console.log(`
-        Title:                    ${nobodyMovie['Title']} (${nobodyMovie['Year']})
-        IMDB Rating:              ${nobodyMovie['imdbRating']}
-        Rotten Tomatoes Rating:   ${nobodyMovie['Ratings'][1]['Value']}
-        Country Produced in:      ${nobodyMovie['Country']}
-        Language:                 ${nobodyMovie['Language']}
-        Actors:                   ${nobodyMovie['Actors']}
-        Plot:                     ${nobodyMovie['Plot']}
-          
-                    `);
-                }
-            })
-            
-            console.log("If you haven't Watch Mr.Nobody you should, it's on Netflix!")
-        }else{
-       
-       console.log(`
-       Title:                    ${movieInfo['Title']} (${movieInfo['Year']})
-       IMDB Rating:              ${movieInfo['imdbRating']}
-       Rotten Tomatoes Rating:   ${movieInfo['Ratings'][1]['Value']}
-       Country Produced in:      ${movieInfo['Country']}
-       Language:                 ${movieInfo['Language']}
-       Actors:                   ${movieInfo['Actors']}
-       Plot:                     \n${movieInfo['Plot']}
-            
-       `)
-    }
-    
-    }
-})
-}
-// var bandArgs = process.argv;
-// var artist = "";
-
-// for(var i=2; i< bandArgs.length; i++){
-//     if (i > 2 && i < bandArgs.length){
-//         artist = artist + "+" + bandArgs[i];
 //     }else{
-//         artist += bandArgs[i];
+//         inputName += userInput[i];
+        
 //     }
 // }
-function concertIt(){
-var bandUrl = "https://rest.bandsintown.com/artists/" + inputName + "/events?app_id=codingbootcamp"
 
-request(bandUrl, function(error, response, body){
+// if (operand === "movie-this"){
+//     movieIt();
+// }else if(operand === "concert-this"){
+//     concertIt();
+
+// }
+// function movieIt(){
+// var movieUrl = "http://www.omdbapi.com/?t="+ inputName + "&apikey=trilogy"
+
+// request(movieUrl, function(error, response, body){
     
-    if(!error && response.statusCode === 200){
-        var artistInfo = JSON.parse(body);
-        console.log(artistInfo)
-        console.log(`
-        Location:      ${artistInfo[0]['venue']['city']}, ${artistInfo[0]['venue']['region']}, ${artistInfo[0]['venue']['country']}
-        Venue Name:    ${artistInfo[0]['venue']['name']},
-        //have to add moment for this to change format to mm/dd/yyyy
-        Event Date:    ${artistInfo[0]['datetime']},
+//     if(!error && response.statusCode === 200){
+//         var movieInfo = JSON.parse(body);
+//         if (inputName == false){
+//             nobodyUrl = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy"
+//             request(nobodyUrl, function(error, response, body){
+//                 if(!error && response.statusCode === 200){
+//                     var nobodyMovie = JSON.parse(body);
+//                     console.log(`
+//         Title:                    ${nobodyMovie['Title']} (${nobodyMovie['Year']})
+//         IMDB Rating:              ${nobodyMovie['imdbRating']}
+//         Rotten Tomatoes Rating:   ${nobodyMovie['Ratings'][1]['Value']}
+//         Country Produced in:      ${nobodyMovie['Country']}
+//         Language:                 ${nobodyMovie['Language']}
+//         Actors:                   ${nobodyMovie['Actors']}
+//         Plot:                     ${nobodyMovie['Plot']}
+          
+//                     `);
+//                 }
+//             })
+            
+//             console.log("If you haven't Watch Mr.Nobody you should, it's on Netflix!")
+//         }else{
+       
+//        console.log(`
+//        Title:                    ${movieInfo['Title']} (${movieInfo['Year']})
+//        IMDB Rating:              ${movieInfo['imdbRating']}
+//        Rotten Tomatoes Rating:   ${movieInfo['Ratings'][1]['Value']}
+//        Country Produced in:      ${movieInfo['Country']}
+//        Language:                 ${movieInfo['Language']}
+//        Actors:                   ${movieInfo['Actors']}
+//        Plot:                     \n${movieInfo['Plot']}
+            
+//        `)
+//     }
+    
+//     }
+// })
+// }
+// // var bandArgs = process.argv;
+// // var artist = "";
 
-        `)
-    }
-})
-}
+// // for(var i=2; i< bandArgs.length; i++){
+// //     if (i > 2 && i < bandArgs.length){
+// //         artist = artist + "+" + bandArgs[i];
+// //     }else{
+// //         artist += bandArgs[i];
+// //     }
+// // }
+// function concertIt(){
+// var bandUrl = "https://rest.bandsintown.com/artists/" + inputName + "/events?app_id=codingbootcamp"
+
+// request(bandUrl, function(error, response, body){
+    
+//     if(!error && response.statusCode === 200){
+//         var artistInfo = JSON.parse(body);
+//         console.log(artistInfo)
+//         console.log(`
+//         Location:      ${artistInfo[0]['venue']['city']}, ${artistInfo[0]['venue']['region']}, ${artistInfo[0]['venue']['country']}
+//         Venue Name:    ${artistInfo[0]['venue']['name']},
+//         //have to add moment for this to change format to mm/dd/yyyy
+//         Event Date:    ${artistInfo[0]['datetime']},
+
+//         `)
+//     }
+// })
+// }
 
 
 
