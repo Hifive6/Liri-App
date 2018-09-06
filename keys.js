ef566e303bc4b80012b22af33f4bc19bb6843846
Hifive6 <39879994+Hifive6@users.noreply.github.com>
2018-09-05 22:29:36 -0400

    troubleshooted spotify api

--- keys.js
+++ keys.js
@@ -1,8 +1,13 @@
+// console.log('this is loaded');
+
+// exports.spotify = {
+//   id: process.env.SPOTIFY_ID,
+//   secret: process.env.SPOTIFY_SECRET
+// };
+
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.d3ee4ebef39140049a7f857cc69a59e7,
-  secret: process.env.b2609640848eafb60769679e53,
-  
-  // b2609640848eafb60769679e53
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -1,38 +1,41 @@
 require("dotenv").config();
 var request = require("request");
+var Spotify = require('node-spotify-api');
 var keyFile = require("./keys.js");
-for (var key in keyFile){
-    console.log(key + keyFile.spotify)
-}
-//var spotify = new spotify(keys.spotify)
+console.log (keyFile)
+// // for (var key in keyFile){
+// //     console.log(key + keyFile.spotify)
+// // }
+var spotify = new Spotify(keyFile.spotify)
+console.log(spotify);
 
-var songArg = process.argv;
-var songName = " ";
+// songArg = process.argv;
+// var songName = " ";
 
-for(var i=0; i< songArg.length; i++){
-        if (i > 3 && i < songArg.length){
-            songName = songName + "+" + songArg[i];
+// for(var i=0; i< songArg.length; i++){
+//          if (i > 3 && i < songArg.length){
+//              songName = songName + "+" + songArg[i];
+//             console.log(songName)
+//          }else{
+//              songName += songArg[i];
             
-        }else{
-            songName += songArg[i];
-            
-        }
-    }
-var spotUrl = "https://api.spotify.com/v1/artists/" + keyFile +"/albums";
-console.log(spotUrl)
-request(spotUrl, function(error, response, body){
+//          }
+//      }
+// var spotUrl = "https://api.spotify.com/v1/6rqhFgbbKwnb9MLmUQDhG6"
+// console.log(spotUrl)
+// request(spotUrl, function(error, response, body){
         
-         if(!error && response.statusCode === 200){
-             var songInfo = JSON.parse(body);
-             console.log(songInfo)
+//          if(!error && response.statusCode === 200){
+//              var songInfo = JSON.parse(body);
+//              console.log(songInfo)
             
-         }
-        })
+//          }
+//         })
         
 
-// var inputString = process.argv;
-// var operand = inputString[2];
-// var userInput = inputString[3];
+var inputString = process.argv;
+var operand = inputString[2];
+var inputName = inputString.slice(3).join(" ");
 
 
 // var inputName = "";
@@ -46,83 +49,83 @@ request(spotUrl, function(error, response, body){
 //     }
 // }
 
-// if (operand === "movie-this"){
-//     movieIt();
-// }else if(operand === "concert-this"){
-//     concertIt();
+if (operand === "movie-this"){
+    movieIt();
+}else if(operand === "concert-this"){
+    concertIt();
 
-// }
-// function movieIt(){
-// var movieUrl = "http://www.omdbapi.com/?t="+ inputName + "&apikey=trilogy"
+}
+function movieIt(){
+var movieUrl = "http://www.omdbapi.com/?t="+ inputName + "&apikey=trilogy"
 
-// request(movieUrl, function(error, response, body){
+request(movieUrl, function(error, response, body){
     
-//     if(!error && response.statusCode === 200){
-//         var movieInfo = JSON.parse(body);
-//         if (inputName == false){
-//             nobodyUrl = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy"
-//             request(nobodyUrl, function(error, response, body){
-//                 if(!error && response.statusCode === 200){
-//                     var nobodyMovie = JSON.parse(body);
-//                     console.log(`
-//         Title:                    ${nobodyMovie['Title']} (${nobodyMovie['Year']})
-//         IMDB Rating:              ${nobodyMovie['imdbRating']}
-//         Rotten Tomatoes Rating:   ${nobodyMovie['Ratings'][1]['Value']}
-//         Country Produced in:      ${nobodyMovie['Country']}
-//         Language:                 ${nobodyMovie['Language']}
-//         Actors:                   ${nobodyMovie['Actors']}
-//         Plot:                     ${nobodyMovie['Plot']}
+    if(!error && response.statusCode === 200){
+        var movieInfo = JSON.parse(body);
+        if (inputName == false){
+            nobodyUrl = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy"
+            request(nobodyUrl, function(error, response, body){
+                if(!error && response.statusCode === 200){
+                    var nobodyMovie = JSON.parse(body);
+                    console.log(`
+        Title:                    ${nobodyMovie['Title']} (${nobodyMovie['Year']})
+        IMDB Rating:              ${nobodyMovie['imdbRating']}
+        Rotten Tomatoes Rating:   ${nobodyMovie['Ratings'][1]['Value']}
+        Country Produced in:      ${nobodyMovie['Country']}
+        Language:                 ${nobodyMovie['Language']}
+        Actors:                   ${nobodyMovie['Actors']}
+        Plot:                     ${nobodyMovie['Plot']}
           
-//                     `);
-//                 }
-//             })
+                    `);
+                }
+            })
             
-//             console.log("If you haven't Watch Mr.Nobody you should, it's on Netflix!")
-//         }else{
+            console.log("If you haven't Watch Mr.Nobody you should, it's on Netflix!")
+        }else{
        
-//        console.log(`
-//        Title:                    ${movieInfo['Title']} (${movieInfo['Year']})
-//        IMDB Rating:              ${movieInfo['imdbRating']}
-//        Rotten Tomatoes Rating:   ${movieInfo['Ratings'][1]['Value']}
-//        Country Produced in:      ${movieInfo['Country']}
-//        Language:                 ${movieInfo['Language']}
-//        Actors:                   ${movieInfo['Actors']}
-//        Plot:                     \n${movieInfo['Plot']}
+       console.log(`
+       Title:                    ${movieInfo['Title']} (${movieInfo['Year']})
+       IMDB Rating:              ${movieInfo['imdbRating']}
+       Rotten Tomatoes Rating:   ${movieInfo['Ratings'][1]['Value']}
+       Country Produced in:      ${movieInfo['Country']}
+       Language:                 ${movieInfo['Language']}
+       Actors:                   ${movieInfo['Actors']}
+       Plot:                     \n${movieInfo['Plot']}
             
-//        `)
-//     }
+       `)
+    }
     
+    }
+})
+}
+// var bandArgs = process.argv;
+// var artist = "";
+
+// for(var i=2; i< bandArgs.length; i++){
+//     if (i > 2 && i < bandArgs.length){
+//         artist = artist + "+" + bandArgs[i];
+//     }else{
+//         artist += bandArgs[i];
 //     }
-// })
 // }
-// // var bandArgs = process.argv;
-// // var artist = "";
+function concertIt(){
+var bandUrl = "https://rest.bandsintown.com/artists/" + inputName + "/events?app_id=codingbootcamp"
 
-// // for(var i=2; i< bandArgs.length; i++){
-// //     if (i > 2 && i < bandArgs.length){
-// //         artist = artist + "+" + bandArgs[i];
-// //     }else{
-// //         artist += bandArgs[i];
-// //     }
-// // }
-// function concertIt(){
-// var bandUrl = "https://rest.bandsintown.com/artists/" + inputName + "/events?app_id=codingbootcamp"
-
-// request(bandUrl, function(error, response, body){
+request(bandUrl, function(error, response, body){
     
-//     if(!error && response.statusCode === 200){
-//         var artistInfo = JSON.parse(body);
-//         console.log(artistInfo)
-//         console.log(`
-//         Location:      ${artistInfo[0]['venue']['city']}, ${artistInfo[0]['venue']['region']}, ${artistInfo[0]['venue']['country']}
-//         Venue Name:    ${artistInfo[0]['venue']['name']},
-//         //have to add moment for this to change format to mm/dd/yyyy
-//         Event Date:    ${artistInfo[0]['datetime']},
+    if(!error && response.statusCode === 200){
+        var artistInfo = JSON.parse(body);
+        console.log(artistInfo)
+        console.log(`
+        Location:      ${artistInfo[0]['venue']['city']}, ${artistInfo[0]['venue']['region']}, ${artistInfo[0]['venue']['country']}
+        Venue Name:    ${artistInfo[0]['venue']['name']},
+        //have to add moment for this to change format to mm/dd/yyyy
+        Event Date:    ${artistInfo[0]['datetime']},
 
-//         `)
-//     }
-// })
-// }
+        `)
+    }
+})
+}
 
 
 
